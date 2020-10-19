f5004c15b98c372adcbc9fbf4a725765bf004a00
Sifan Jiang <jasper.sifan@gmail.com>
2020-10-19 15:22:31 +0800

    Sorted imports.

--- pytracking/run_tracker.py
+++ pytracking/run_tracker.py
@@ -1,14 +1,13 @@
+import argparse
 import os
 import sys
-import argparse
 
 env_path = os.path.join(os.path.dirname(__file__), '..')
 if env_path not in sys.path:
     sys.path.append(env_path)
 
-from pytracking.evaluation import get_dataset
+from pytracking.evaluation import Tracker, get_dataset
 from pytracking.evaluation.running import run_dataset
-from pytracking.evaluation import Tracker
 
 
 def run_tracker(tracker_name, tracker_param, run_id=None, dataset_name='otb', sequence=None, debug=0, threads=0,
