0c6ab6d3aa6d58173673d4ff520f6491cd1ec6c2
Sifan Jiang <jasper.sifan@gmail.com>
2020-10-26 11:43:20 +0800

    Save output videos with detailed flag

--- pytracking/create_videos.py
+++ pytracking/create_videos.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import sys
+
 from cv2 import cv2 as cv
 
 env_path = os.path.join(os.path.dirname(__file__), '..')
@@ -42,14 +43,16 @@ def convert_images_to_video(image_path, video_path, fps=30.0, frameSize=None, im
     :type image_type: str, optional
     """
     image_list = get_image_list(image_path, image_type)
-    if not len(image_list): return
+    if not len(image_list):
+        return
     if not frameSize:
         image = cv.imread(image_list[0])
         height, width, _ = image.shape
         frameSize = (width, height)
 
     fourcc = cv.VideoWriter_fourcc(*'MJPG')
-    video = cv.VideoWriter(video_path, fourcc=fourcc, fps=fps, frameSize=frameSize)
+    video = cv.VideoWriter(video_path, fourcc=fourcc,
+                           fps=fps, frameSize=frameSize)
     for image_name in image_list:
         image = cv.imread(image_name)
         video.write(image)
@@ -58,7 +61,7 @@ def convert_images_to_video(image_path, video_path, fps=30.0, frameSize=None, im
     cv.destroyAllWindows()
 
 
-def overlap_bbox_on_video(bbox_path, video_path, out_path=None, gt_path=None, fps=30.0, flag=''):
+def overlap_bbox_on_video(bbox_path, video_path, out_path=None, search_area_path=None, gt_path=None, fps=30.0, flag=''):
     """[summary]
 
     :param bbox_path: [description]
@@ -67,15 +70,20 @@ def overlap_bbox_on_video(bbox_path, video_path, out_path=None, gt_path=None, fp
     :type video_path: [type]
     :param out_path: [description], defaults to None
     :type out_path: [type], optional
-    """    
+    """
     if not out_path:
         video_path_split = video_path.split('.')
+        if search_area_path != None:
+            video_path_split[-2] += "+Search_Area"
         video_path_split[-2] += "+BBox" + flag
         out_path = '.'.join(video_path_split)
         out_path = out_path.replace('mp4', 'avi')
 
     bbox_file = open(bbox_path)
     bbox_lines = bbox_file.readlines()
+
+    if search_area_path != None:
+        search_area_file = open(search_area_path)
 
     video = cv.VideoCapture(video_path)
     ret, frame = video.read()
@@ -84,29 +92,47 @@ def overlap_bbox_on_video(bbox_path, video_path, out_path=None, gt_path=None, fp
     frameSize = (width, height)
 
     fourcc = cv.VideoWriter_fourcc(*'MJPG')
-    output = cv.VideoWriter(out_path, fourcc=fourcc, fps=fps, frameSize=frameSize)
+    output = cv.VideoWriter(out_path, fourcc=fourcc,
+                            fps=fps, frameSize=frameSize)
 
     for i, line in enumerate(bbox_lines):
         if i != 0:
             ret, frame = video.read()
 
+        frame_copy = frame.copy()
+
+        if search_area_path != None:
+            search_area_line = search_area_file.readline()
+            if search_area_line != '':
+                l, b, w, h = tuple(map(int, search_area_line.split()))
+                left_corner, right_corner = (l, b), (l+w, b+h)
+                cv.rectangle(frame_copy, left_corner, right_corner,
+                             color=(0, 0, 255), thickness=2)
+
         l, b, w, h = tuple(map(int, line.split()))
         left_corner, right_corner = (l, b), (l+w, b+h)
 
-        frame_bbox = cv.rectangle(frame, left_corner, right_corner, color=(0, 255, 0), thickness=2)
-        output.write(frame_bbox)
+        cv.rectangle(frame_copy, left_corner, right_corner,
+                     color=(0, 255, 0), thickness=2)
+        output.write(frame_copy)
 
     bbox_file.close()
+    if search_area_path != None:
+        search_area_file.close()
     video.release()
     output.release()
     cv.destroyAllWindows()
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Run converter from images to videos.')
-    parser.add_argument('--tracker_name', type=str, default='dimp',help='Name of tracking method.')
-    parser.add_argument('--tracker_param', type=str, default='dimp50', help='Name of parameter file.')
-    parser.add_argument('--create_method', type=str, default='bbox2video', help='Name of the video generation method.')
+    parser = argparse.ArgumentParser(
+        description='Run converter from images to videos.')
+    parser.add_argument('--tracker_name', type=str,
+                        default='dimp', help='Name of tracking method.')
+    parser.add_argument('--tracker_param', type=str,
+                        default='dimp50', help='Name of parameter file.')
+    parser.add_argument('--create_method', type=str, default='bbox2video',
+                        help='Name of the video generation method.')
 
     args = parser.parse_args()
 
@@ -127,15 +153,22 @@ def main():
             if os.path.isfile(os.path.join(tracking_results_path, filename)):
                 del file_list[i]
 
-        path_list = list(map(lambda x: os.path.join(tracking_results_path, x), file_list))
+        path_list = list(map(lambda x: os.path.join(
+            tracking_results_path, x), file_list))
 
         for file, path in zip(file_list, path_list):
-            convert_images_to_video(path, path+'/'+file+'.avi', fps=30.0, frameSize=None, image_type='jpg')
+            convert_images_to_video(
+                path, path+'/'+file+'.avi', fps=30.0, frameSize=None, image_type='jpg')
     elif args.create_method == 'bbox2video':
-        print("Creating video from txt.")
+        print("Creating video from bounding boxes.")
         bbox_path = '/home/sifan/Documents/pytracking/pytracking/tracking_results/dimp/dimp50/video_Office_001_960x540.txt'
         video_path = '/home/sifan/Documents/pytracking/pytracking/datasets/Videos/Office/Office_001_960x540.mp4'
-        overlap_bbox_on_video(bbox_path, video_path, flag='sample_memory_size=30')
+        search_area_path = '/home/sifan/Documents/pytracking/pytracking/tracking_results/dimp/dimp50/video_Office_001_960x540_Search_Area.txt'
+        sample_memory_size = input("What is the sample memory size: ")
+        search_area_scale = input("What is the search area scale: ")
+        overlap_bbox_on_video(bbox_path, video_path, search_area_path=search_area_path,
+                              flag='+Sample_Memory_size={}+Search_Area_Scale={}'.format(sample_memory_size, search_area_scale))
+
 
 if __name__ == '__main__':
     main()
